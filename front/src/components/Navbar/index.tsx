73d40eab53f57648b924e8911c0ea89a6473c21c
DenerBorges <denerkborges@bol.com.br>
2024-11-11 16:23:55 -0300

    feat: creating navbar component

---
+++ front/src/components/Navbar/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Container, Logo, ProfileImage } from "./styles";
+
+interface NavbarProps {
+  transparent?: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ transparent }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Container transparent={transparent}>
+      <Logo
+        src={require("../../assets/images/logo192.png")}
+        alt="Logo do App"
+      />
+      <ProfileImage
+        src={require("../../assets/images/White_Facebook.png")}
+        alt="Foto de perfil"
+        onClick={() => navigate("/profile")}
+      />
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ front/src/components/Navbar/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { COLORS } from "../../assets/colors";
+
+export const Container = styled.div<{ transparent?: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 2rem 2.2rem;
+  background-color: ${({ transparent }) =>
+    transparent ? "transparent" : COLORS.white};
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+
+  box-shadow: ${({ transparent }) =>
+    transparent ? "none" : "0 2px 4px rgba(0, 0, 0, 0.1)"};
+`;
+
+export const Logo = styled.img`
+  width: 45px;
+  height: 45px;
+`;
+
+export const ProfileImage = styled.img`
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  cursor: pointer;
+  object-fit: cover;
+`;
