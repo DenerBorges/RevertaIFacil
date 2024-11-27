d3dde30e9706b737b30a028901847e8ec9fa4a4e
DenerBorges <denerkborges@bol.com.br>
2024-11-27 16:04:21 -0300

    feat: creating user type

---
+++ front/src/types/user.ts
@@ -0,0 +1,20 @@
+export interface userType {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  profilePic: string;
+  mobile: string;
+  document: string;
+  zipCode: string;
+  street: string;
+  number: string;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  isClient: boolean;
+  isCollector: boolean;
+  createdAt: Date;
+}
